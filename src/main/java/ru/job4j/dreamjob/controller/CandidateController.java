e89e065d529df5a13c00f5173f03bb9339ee49bd
konstantin <kotmix@bk.ru>
2022-05-11 20:48:27 +0400

    8. HTML form. Создание кандидата. [#504845] + fix 6. HTML Link [#504843 #299514] + fix 7. HTML form. Создание вакансии. [#504844 #299579]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.dream.model.candidate.Candidate;
 import ru.job4j.dreamjob.dream.model.candidate.CandidateStore;
 
 @Controller
@@ -11,8 +12,15 @@ public class CandidateController {
     private final CandidateStore store = CandidateStore.instOf();
 
     @GetMapping("/candidates")
-    public String posts(Model model) {
+    public String candidates(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("candidate", new Candidate(0, "Заполните поле", "Заполните поле",
+                "Заполните поле"));
+        return "addCandidate";
     }
 }
