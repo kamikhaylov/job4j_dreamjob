05f8acdee0eca6ef555a7eda45422a58a8201795
konstantin <kotmix@bk.ru>
2022-05-16 20:55:45 +0400

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,12 +3,18 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.dream.model.candidate.Candidate;
 import ru.job4j.dreamjob.dream.model.candidate.CandidateStore;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
+
 @Controller
 public class CandidateController {
-
+    private static final Logger LOGGER = Logger.getLogger(CandidateController.class.getName());
     private final CandidateStore store = CandidateStore.instOf();
 
     @GetMapping("/candidates")
@@ -22,5 +28,26 @@ public class CandidateController {
         model.addAttribute("candidate", new Candidate(0, "Заполните поле", "Заполните поле",
                 "Заполните поле"));
         return "addCandidate";
+    }
+
+    @PostMapping("/createCandidate")
+    public String createPost(HttpServletRequest req) {
+        String name = req.getParameter("name");
+        String description = req.getParameter("description");
+        LOGGER.info("name : " + name + ", description : " + description);
+        store.add(name, description);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/formUpdateCandidate/{candidateId}")
+    public String formUpdatePost(Model model, @PathVariable("candidateId") int id) {
+        model.addAttribute("candidate", store.findById(id));
+        return "updateCandidate";
+    }
+
+    @PostMapping("/updateCandidate")
+    public String updateCandidate(@ModelAttribute Candidate candidate) {
+        store.update(candidate);
+        return "redirect:/candidates";
     }
 }
--- src/main/java/ru/job4j/dreamjob/dream/model/candidate/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/dream/model/candidate/CandidateStore.java
@@ -1,13 +1,20 @@
 package ru.job4j.dreamjob.dream.model.candidate;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
 public class CandidateStore {
+    private static final Logger LOGGER = Logger.getLogger(CandidateStore.class.getName());
     private static final CandidateStore INST = new CandidateStore();
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final AtomicInteger atomicInteger = new AtomicInteger(3);
 
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Petr", "Junior Java Job", "2022-01-01"));
@@ -21,5 +28,30 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    public boolean add(String name, String description) {
+        int indexId = atomicInteger.incrementAndGet();
+        candidates.put(indexId, new Candidate(indexId, name, description, createDate()));
+        LOGGER.info("indexId : " + indexId + ", name : " + name + ", description : " + description);
+        return true;
+    }
+
+    public boolean update(Candidate candidate) {
+        candidates.put(candidate.getId(), new Candidate(candidate.getId(), candidate.getName(),
+                candidate.getDescription(), candidates.get(candidate.getId()).getCreated()));
+        return true;
+    }
+
+    private String createDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        String date = dateFormat.format(calendar.getTime());
+        LOGGER.info("createDate : " + date);
+        return date;
     }
 }
