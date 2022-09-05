4aaf327f4163863f8ba112cd6ee0f376aecb172a
konstantin <kotmix@bk.ru>
2022-09-05 10:23:52 +0400

    3. Filter [#504865 #339583] Правки по ревью

--- src/main/java/ru/job4j/dreamjob/dream/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/dream/filter/AuthFilter.java
@@ -12,28 +12,52 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 import static java.util.Objects.isNull;
 
 @Component
 public class AuthFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthFilter.class.getName());
+    private static final String USER = "user";
+    private static final String LOGIN_PAGE = "/loginPage";
+
+    private final Set<String> addresses = Set.of(
+            "formAddUser",
+            "registration",
+            "success",
+            "fail",
+            "loginPage",
+            "login"
+    );
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
         LOGGER.info("AuthFilter.doFilter");
+
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (addressPresent(uri)) {
             chain.doFilter(req, res);
             return;
         }
-        if (isNull(req.getSession().getAttribute("user"))) {
-            res.sendRedirect(req.getContextPath() + "/loginPage");
+        if (isNull(req.getSession().getAttribute(USER))) {
+            res.sendRedirect(req.getContextPath() + LOGIN_PAGE);
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean addressPresent(String uri) {
+        boolean result = false;
+        for (String address : addresses) {
+            if (uri.endsWith(address)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
